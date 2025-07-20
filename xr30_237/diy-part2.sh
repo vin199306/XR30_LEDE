e8a680d1696c51012763dbbac24927f3f2ef060a
Yanwen <66867243+vin199306@users.noreply.github.com>
2025-07-20 16:13:46 +0800

    aa

--- xr30_237/diy-part2.sh
+++ xr30_237/diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sudo apt install libfuse-dev
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
